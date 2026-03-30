31121f62aff0fdee6e33b373a956366ff2bf09f5
Vincent Danchaud <vdanchaud@gmail.com>
2026-03-30 18:39:53 +0200

    add throw error when db not connected

--- src/database/db.ts
+++ src/database/db.ts
@@ -14,5 +14,6 @@ export const initDB = async () => {
     console.log('Connection has been established successfully');
   } catch (error) {
     console.error('Unable to connect to the database', error);
+    throw error;
   }
 };
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import { config } from 'dotenv';
 import { buildApp } from './app/app';
+import { initDB } from './database/db';
 
 config();
 const PORT = process.env.PORT || 3000;
 
-const app = buildApp();
+initDB()
+  .then(() => {
+    const app = buildApp();
 
-app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`);
-});
+    app.listen(PORT, () => {
+      console.log(`Listening on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Cannot start server, DB not connected', err);
+  });
