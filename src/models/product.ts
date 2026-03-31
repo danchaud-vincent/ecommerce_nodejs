54cb235056dcdce08ba3cc906a24cfcd0e99c58d
Vincent Danchaud <vdanchaud@gmail.com>
2026-03-31 12:41:18 +0200

    modify product model by adding interface and typescript schemaé

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,29 +1,49 @@
-import { DataTypes } from 'sequelize';
+import { DataTypes, Model } from 'sequelize';
 import { sequelize } from '../database/db';
 
-export const Product = sequelize.define('product', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    allowNull: false,
-    autoIncrement: true,
-  },
-  name: {
-    type: DataTypes.STRING(100),
-    allowNull: false,
-  },
-  price: {
-    type: DataTypes.DOUBLE,
-    allowNull: false,
-  },
-  description: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  imageUrl: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
+interface ProductAttributes {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  imageUrl: string;
+}
 
-export default Product;
+export class Product
+  extends Model<ProductAttributes>
+  implements ProductAttributes
+{
+  id!: number;
+  name!: string;
+  price!: number;
+  description!: string;
+  imageUrl!: string;
+}
+
+Product.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DOUBLE,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    imageUrl: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  { sequelize, tableName: 'products' },
+);
