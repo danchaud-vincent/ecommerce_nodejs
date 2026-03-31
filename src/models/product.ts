acd9d5abd798c99550ed578862f9895d8ea8eb0e
Vincent Danchaud <vdanchaud@gmail.com>
2026-03-31 11:44:30 +0200

    create a Product model with sequelize

---
+++ src/models/product.ts
@@ -0,0 +1,29 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../database/db';
+
+export const Product = sequelize.define('product', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.DOUBLE,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+export default Product;
