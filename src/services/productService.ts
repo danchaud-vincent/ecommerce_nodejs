410f506728e1cdaa9ef896fb1e672bde7716fde1
Vincent Danchaud <vdanchaud@gmail.com>
2026-03-31 13:14:55 +0200

    update product service

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,5 +1,10 @@
 import { Product } from '../models/product';
+import { ProductRepository } from '../repositories/productRepository';
 
 export class ProductService {
-  constructor() {}
+  constructor(private productRepository: ProductRepository) {}
+
+  getProducts(): Promise<Product[]> {
+    return this.productRepository.findAll();
+  }
 }
