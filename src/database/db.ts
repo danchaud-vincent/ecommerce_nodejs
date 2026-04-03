c5f89b98ea37ccd8cec10cbaae2287b23ad6a012
Vincent Danchaud <vdanchaud@gmail.com>
2026-04-03 17:07:46 +0200

    correct config files by adding a sequelize file to config the db, adding a hook to create a stock when a product is created by the admin

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,19 +1,11 @@
-import { dbConfig } from '../config/db';
-import { Sequelize } from 'sequelize';
 import { Product } from '../models/product';
 import { Stock } from '../models/stock';
+import { sequelize } from './sequelize';
 
 function setDatabaseAssociations() {
   Product.hasOne(Stock, { foreignKey: 'productId', as: 'stock' });
   Stock.belongsTo(Product, { foreignKey: 'productId', as: 'product' });
 }
-
-export const sequelize = new Sequelize(
-  dbConfig.database,
-  dbConfig.username,
-  dbConfig.password,
-  { dialect: 'mysql', host: dbConfig.host },
-);
 
 export const initDB = async () => {
   try {
@@ -23,6 +15,7 @@ export const initDB = async () => {
     setDatabaseAssociations();
 
     await sequelize.sync({ force: true });
+
     console.log('All models were synchronized successfully.');
   } catch (error) {
     console.error('Unable to connect to the database', error);
--- src/models/stock.ts
+++ src/models/stock.ts
@@ -1,5 +1,5 @@
-import { Model } from 'sequelize';
-import { sequelize } from '../database/db';
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from '../database/sequelize';
 
 interface StockAttributes {
   id: number;
@@ -7,26 +7,31 @@ interface StockAttributes {
   quantity: number;
 }
 
-export class Stock extends Model<StockAttributes> implements StockAttributes {
-  id!: number;
-  productId!: number;
-  quantity!: number;
+export interface StockCreationAttributes extends Omit<StockAttributes, 'id'> {}
+
+export class Stock
+  extends Model<StockAttributes, StockCreationAttributes>
+  implements StockAttributes
+{
+  declare id: number;
+  declare productId: number;
+  declare quantity: number;
 }
 
 Stock.init(
   {
     id: {
-      type: 'INTEGER',
+      type: DataTypes.INTEGER,
       primaryKey: true,
       allowNull: false,
       autoIncrement: true,
     },
     productId: {
-      type: 'INTEGER',
+      type: DataTypes.INTEGER,
       allowNull: false,
     },
     quantity: {
-      type: 'INTEGER',
+      type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 0,
     },
