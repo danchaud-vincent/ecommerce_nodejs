5e5c2e22683bfc807b392a66029c9e033f738a25
Vincent Danchaud <vdanchaud@gmail.com>
2026-04-08 18:58:08 +0200

    import the cartProductRepo in the cart routes

--- src/routes/user/cart.ts
+++ src/routes/user/cart.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import { CartController } from '../../controllers/cartController';
 import { CartService } from '../../services/cartServices';
 import { CartRepository } from '../../repositories/cartRepository';
+import { CartProductRepository } from '../../repositories/cartProductRepository';
 
 const cartRoutes = express.Router();
 
 const cartRepository = new CartRepository();
-const cartService = new CartService(cartRepository);
+const cartProductRepository = new CartProductRepository();
+const cartService = new CartService(cartRepository, cartProductRepository);
 const cartController = new CartController(cartService);
 
 cartRoutes.get('/cart', cartController.getCart);
