db245a08a7c3df22feb46f65292e8cc4889f2197
Vincent Danchaud <vdanchaud@gmail.com>
2026-04-10 16:22:29 +0200

    add trycatch block in cartcontroller and remove useless trycath in cart service

--- src/controllers/cartController.ts
+++ src/controllers/cartController.ts
@@ -21,34 +21,44 @@ export class CartController {
   };
 
   addProductToCart = async (req: Request, res: Response) => {
-    const userId = req.user?.id;
-    const productId = req.body.productId;
-    const quantity = req.body.quantity;
+    try {
+      const userId = req.user?.id;
+      const productId = req.body.productId;
+      const quantity = req.body.quantity;
 
-    if (!userId) {
-      return res.status(401).json({ message: 'Unauthorized' });
+      if (!userId) {
+        return res.status(401).json({ message: 'Unauthorized' });
+      }
+
+      const result = await this.cartService.updateProductQuantity(
+        productId,
+        userId,
+        quantity,
+      );
+      res.status(200).json(result);
+    } catch (err: any) {
+      console.error(err);
+      res.status(500).json({ message: err.message });
     }
-
-    const result = await this.cartService.updateProductQuantity(
-      productId,
-      userId,
-      quantity,
-    );
-    res.status(200).json(result);
   };
 
   removeProductFromCart = async (req: Request, res: Response) => {
-    const userId = req.user?.id;
-    const productId = Number(req.params.id);
+    try {
+      const userId = req.user?.id;
+      const productId = Number(req.params.id);
 
-    if (!userId) {
-      return res.status(401).json({ message: 'Unauthorized' });
+      if (!userId) {
+        return res.status(401).json({ message: 'Unauthorized' });
+      }
+
+      const result = await this.cartService.deleteProductFromCart(
+        productId,
+        userId,
+      );
+      res.status(200).json(result);
+    } catch (err: any) {
+      console.error(err);
+      res.status(500).json({ message: err.message });
     }
-
-    const result = await this.cartService.deleteProductFromCart(
-      productId,
-      userId,
-    );
-    res.status(200).json(result);
   };
 }
--- src/services/cartServices.ts
+++ src/services/cartServices.ts
@@ -10,6 +10,7 @@ export class CartService {
 
   async getCartByUserId(userId: number): Promise<Cart> {
     let cart = await this.cartRepository.getCartByUserId(userId);
+
     if (!cart) {
       // If no cart exists for the user, create an empty one
       cart = await this.cartRepository.createCartForUser(userId);
@@ -22,78 +23,64 @@ export class CartService {
     userId: number,
     quantity: number,
   ): Promise<Cart> {
-    try {
-      const product = await this.productRepository.findByID(productId);
+    const product = await this.productRepository.findByID(productId);
 
-      if (!product) {
-        throw new Error(`Product ${productId} does not exist in the catalog.`);
-      }
-
-      const cart = await this.getCartByUserId(userId);
-      const existingProductInCart = await this.cartRepository.getProductInCart(
-        cart.id,
-        productId,
-      );
-
-      if (!existingProductInCart) {
-        if (quantity > 0) {
-          await this.cartRepository.addProductToCart(
-            cart.id,
-            productId,
-            quantity,
-          );
-        } else {
-          throw new Error(
-            'Cannot decrement a product that is not in the cart.',
-          );
-        }
-      } else {
-        const newQuantity = existingProductInCart.quantity + quantity;
-
-        if (newQuantity > 0) {
-          await this.cartRepository.updateProductQuantity(
-            cart.id,
-            productId,
-            quantity,
-          );
-        } else {
-          await this.cartRepository.removeProductFromCart(cart.id, productId);
-        }
-      }
-
-      const updatedCart = this.getCartByUserId(userId);
-
-      return updatedCart;
-    } catch (error: any) {
-      console.error(error.message);
-      throw new Error(`Failed to update the quantity -> ${error.message}`);
+    if (!product) {
+      throw new Error(`Product ${productId} does not exist in the catalog.`);
     }
+
+    const cart = await this.getCartByUserId(userId);
+    const existingProductInCart = await this.cartRepository.getProductInCart(
+      cart.id,
+      productId,
+    );
+
+    if (!existingProductInCart) {
+      if (quantity > 0) {
+        await this.cartRepository.addProductToCart(
+          cart.id,
+          productId,
+          quantity,
+        );
+      } else {
+        throw new Error('Cannot decrement a product that is not in the cart.');
+      }
+    } else {
+      const newQuantity = existingProductInCart.quantity + quantity;
+
+      if (newQuantity > 0) {
+        await this.cartRepository.updateProductQuantity(
+          cart.id,
+          productId,
+          quantity,
+        );
+      } else {
+        await this.cartRepository.removeProductFromCart(cart.id, productId);
+      }
+    }
+
+    const updatedCart = this.getCartByUserId(userId);
+
+    return updatedCart;
   }
 
   async deleteProductFromCart(
     productId: number,
     userId: number,
   ): Promise<{ message: string }> {
-    try {
-      const cart = await this.getCartByUserId(userId);
+    const cart = await this.getCartByUserId(userId);
 
-      const existingProductInCart = await this.cartRepository.getProductInCart(
-        cart.id,
-        productId,
-      );
+    const existingProductInCart = await this.cartRepository.getProductInCart(
+      cart.id,
+      productId,
+    );
 
-      if (!existingProductInCart) {
-        throw new Error('Product not found in cart.');
-      }
-
-      await this.cartRepository.removeProductFromCart(cart.id, productId);
-
-      return { message: `Product with id ${productId} deleted.` };
-    } catch (error: any) {
-      throw new Error(
-        'Failed to remove the product from the cart:',
-        error.message,
-      );
+    if (!existingProductInCart) {
+      throw new Error('Product not found in cart.');
     }
+
+    await this.cartRepository.removeProductFromCart(cart.id, productId);
+
+    return { message: `Product with id ${productId} deleted.` };
   }
 }
