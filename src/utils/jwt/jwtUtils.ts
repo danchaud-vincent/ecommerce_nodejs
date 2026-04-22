bfe71115b50436d22289e9bc722230fd68110f6b
Vincent Danchaud <vdanchaud@gmail.com>
2026-04-22 18:21:01 +0200

    update jwtUtils by adding verifyRefreshToken and verifyAccessToken with types

--- src/utils/jwt/jwtUtils.ts
+++ src/utils/jwt/jwtUtils.ts
@@ -1,11 +1,11 @@
 import type { User } from '../../models/user';
 import { jwtConfig } from '../../config/jwt';
-import jwt, { type SignOptions } from 'jsonwebtoken';
+import jwt, { type JwtPayload, type SignOptions } from 'jsonwebtoken';
 
 export const generateAccessToken = (payload: {
   email: string;
   password: string;
-}) => {
+}): string => {
   return jwt.sign(payload, jwtConfig.jwtSecret, {
     expiresIn: jwtConfig.jwtExpiration as any,
   });
@@ -14,8 +14,16 @@ export const generateAccessToken = (payload: {
 export const generateRefreshToken = (payload: {
   email: string;
   password: string;
-}) => {
+}): string => {
   return jwt.sign(payload, jwtConfig.jwtRefreshSecret, {
     expiresIn: jwtConfig.jwtRefreshExpiration as any,
   });
+};
+
+export const verifyAccessToken = (token: string): JwtPayload => {
+  return jwt.verify(token, jwtConfig.jwtSecret) as JwtPayload;
+};
+
+export const verifyRefreshToken = (token: string): JwtPayload => {
+  return jwt.verify(token, jwtConfig.jwtRefreshSecret) as JwtPayload;
 };
