ba1470baf09ee1d7062b8af8da2aae0fba79e518
Vincent Danchaud <vdanchaud@gmail.com>
2026-04-22 17:39:09 +0200

    import auth controller, service and user repo

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import { AuthController } from '../controllers/authController';
+import { AuthService } from '../services/authService';
+import { UserRepository } from '../repositories/authRepository';
 
 const authRoutes = express.Router();
 
-const authController = new AuthController();
+const authRepository = new UserRepository();
+const authService = new AuthService(authRepository);
+const authController = new AuthController(authService);
 
 authRoutes.get('/login', authController.login);
 
