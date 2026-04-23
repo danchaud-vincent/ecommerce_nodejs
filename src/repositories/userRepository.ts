11eefc09ce19e94225d1e9d98b1f3146f87f576b
Vincent Danchaud <vdanchaud@gmail.com>
2026-04-23 10:12:59 +0200

    add a create method in user repo

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -1,9 +1,13 @@
-import { User } from '../models/user';
+import { User, type UserCreationAttributes } from '../models/user';
 
 export class UserRepository {
   async findByEmail(email: string): Promise<User | null> {
     return User.findOne({
       where: { email },
     });
+  }
+
+  async create(userData: UserCreationAttributes): Promise<User> {
+    return User.create(userData);
   }
 }
